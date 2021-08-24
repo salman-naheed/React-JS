72d3c52f337d968857ce0a3d0d1b9d7afe2c26c4
salman-naheed <salmannaheed23@gmail.com>
2021-08-25 01:36:53 +0500

    nested components

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function Greeting(){
-  return <h4>Hello</h4>
+import logo192 from './logo192.png'
+function BookList() {
+  return (
+    <section>
+      <Book/>
+    </section>
+  )
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById("root"));
+const Book = () => {
+  return(
+    <article>
+      <Image/>,
+      <Title/>
+    </article>
+  )
+}
+
+const Image =()=> {
+  return(
+    <img src={logo192}/>
+  )
+}
+
+const Title = () => {
+  return <h2>Not to give.</h2>
+}
+ReactDOM.render(<BookList />, document.getElementById("root"));
